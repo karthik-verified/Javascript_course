86780745cac920218c501f597cbb7c38007339fc
Karthik Budida <karthikbudida143@gmail.com>
2025-06-25 23:32:38 +0530

    Initial commit

---
+++ rock_paper_scissors.js
@@ -0,0 +1,88 @@
+ let user_choice = 'Rock';
+        
+        let result = 'Win';
+
+        let counter = JSON.parse(localStorage.getItem('score')) || {
+            wins : 0,
+            losses : 0,
+            tie : 0
+        };
+        update_score(result);
+
+        function win_counter (result) {
+            if (result==='Win') {
+            counter.wins +=1;
+        } else if(result==='Loss') {
+            counter.losses +=1;
+        } else if (result==='Tie') {
+            counter.tie +=1;
+        }
+        }
+        
+        function chosen_moves(user_choice,comp_choice) {
+            document.querySelector('.your_choice').innerHTML = `<img src="${user_choice}-emoji.png" height="50px" width="50px">`;
+            document.querySelector('.comp_choice').innerHTML = `<img src="${comp_choice}-emoji.png" height="50px" width="50px">`;
+        }
+        function game_result(result) {
+            if (result==='Win') {
+                document.querySelector('.js-win_or_lose').innerHTML = `You have Won !`;
+            } else if (result==='Loss') {
+                document.querySelector('.js-win_or_lose').innerHTML = `You have Lost `;
+            } else if (result==='Tie') {
+                document.querySelector('.js-win_or_lose').innerHTML = `It is a Tie `;
+            }else {
+                document.querySelector('.js-win_or_lose').innerHTML = `You have ? `;
+            }
+        }
+        
+        
+        function comp_choice_generator() {
+            const number = Math.random();
+            let comp_choice = 'rock';
+            if (number>0 && number<1/3) {
+                comp_choice = 'Rock';
+            } else if(number>1/3 && number<2/3) {
+                comp_choice = 'Paper';
+            } else {
+                comp_choice = 'Scissors';
+            }
+            return comp_choice;
+        }
+        
+        function win_checker(user_choice,comp_choice) {
+            let result = 'Win'
+            if(user_choice==='Rock') {
+                if(comp_choice==='Rock') {
+                    result = 'Tie';
+                } else if (comp_choice==='Paper') {
+                    result = 'Loss';
+                } else if (comp_choice==='Scissors') {
+                    result = 'Win';
+                }
+            }
+           else if(user_choice==='Paper') {
+                if(comp_choice==='Rock') {
+                    result = 'Win';
+                } else if (comp_choice==='Paper') {
+                    result = 'Tie';
+                } else if (comp_choice==='Scissors') {
+                    result = 'Loss';
+                }
+            }
+           else if(user_choice==='Scissors') {
+                if(comp_choice==='Rock') {
+                    result = 'Loss';
+                } else if (comp_choice==='Paper') {
+                    result = 'Win';
+                } else if (comp_choice==='Scissors') {
+                    result = 'Tie';
+                }
+            
+            }
+            return result;
+        }
+        
+
+      function update_score() {
+        document.querySelector('.js-score').innerHTML = `Wins : ${counter.wins} , Losses : ${counter.losses} , Ties : ${counter.tie}`;
+      }
---
+++ trail.py
@@ -0,0 +1,2 @@
+print("hello world")
+print("hi guys this B.Karthik and i am from NIT Warangal , ECE department in Warangal ")
