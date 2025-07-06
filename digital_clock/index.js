240c36fc2fe122d14188568197187f99471e8ae4
Karthik Budida <karthikbudida143@gmail.com>
2025-07-06 13:20:28 +0530

    your update message

--- digital_clock/index.js
+++ digital_clock/index.js
@@ -3,12 +3,14 @@ function update_clock() {
     const hours = now.hour().toString().padStart(2,'0');
     const minutes = now.minute().toString().padStart(2,'0');
     const seconds = now.second().toString().padStart(2,'0');
+    const milliseconds = now.millisecond().toString().padStart(2,'0');
 
     document.body.querySelector('.hour-container').innerHTML = hours;
     document.body.querySelector('.minute-container').innerHTML = minutes;
     document.body.querySelector('.second-container').innerHTML = seconds;
+    document.body.querySelector('.millisecond-container').innerHTML = milliseconds;
 }
 
 setInterval(function() {
     update_clock();
-},1000)
+},1)
